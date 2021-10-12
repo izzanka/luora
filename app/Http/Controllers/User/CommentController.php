25b85638e3a613a322c207b2ea0fd6556c98c334
izzanka <izzancoc.46@gmail.com>
2021-10-12 19:02:20 +0700

    add update,destroy and report function

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Models\Answer;
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Models\ReportComment;
 use App\Http\Controllers\Controller;
 
 class CommentController extends Controller
@@ -21,14 +22,6 @@ class CommentController extends Controller
         return back();
     }
 
-    public function update(){
-
-    }
-
-    public function destroy(){
-        
-    }
-
     // public function replyStore(Request $request)
     // {
     //     $reply = new Comment();
@@ -41,4 +34,55 @@ class CommentController extends Controller
     //     $answer->comments()->save($reply);
     //     return back();
     // }
+
+    public function update(Request $request,Comment $comment){
+
+        $comment->update([
+            'comment' => $request->comment
+        ]);
+
+        return back()->with('message',['text' => 'Comment updated succesfully!', 'class' => 'success']);
+
+    }
+
+    public function destroy(Comment $comment){
+
+        $reports = ReportComment::where('comment_id',$comment->id)->get();
+
+        foreach($reports as $report){
+            $report->delete();
+        }
+
+        $comment->delete();
+
+        return back()->with('message',['text' => 'Comment deleted successfully!', 'class' => 'success']);
+
+    }
+
+    public function report(Request $request,Comment $comment){
+
+        $user_id = auth()->id();
+        $report = ReportComment::where('user_id',$user_id)->where('comment_id',$comment->id)->first();
+
+        if($comment->user_id == $user_id){
+            return back();
+        }
+
+        if($report){
+            return back()->with('message',['text' => 'Comment already reported!', 'class' => 'danger']);
+        }else{
+
+            ReportComment::create([
+                'user_id' => $user_id,
+                'comment_id' => $comment->id,
+                'type' => $request->type,
+            ]);
+
+            return back()->with('message',['text' => 'Comment reported successfully!', 'class' => 'success']);
+        }
+ 
+
+    }
+
+
 }
