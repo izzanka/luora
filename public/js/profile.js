29fe400c1f8416806f7bf5b65ad10641fbb74f0f
izzanka <izzancoc.46@gmail.com>
2021-11-17 16:12:45 +0700

    update js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,108 +1,135 @@
 //profile show
-$('.ajax-loading-2').hide();
-$('#noData').hide();
+$(".ajax-loading-2").hide();
+$("#noData").hide();
 
-$(document).on('click','#showTopics',function(e){
-    $('#noData').hide();
+$(document).on("click", "#showTopics", function (e) {
+    $("#noData").hide();
     $("#showQuestionsHtml").hide();
     $("#showAnswersHtml").hide();
     $("#showTopicsHtml").show();
     e.preventDefault();
-    let site_url = $(this).attr('data-href');
+    let site_url = $(this).attr("data-href");
     let html = "";
     $.ajax({
         url: site_url,
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function(){
-            $('.ajax-loading-2').show();
-        }
-    })
-    .done(function(data){
-        $('.ajax-loading-2').hide();
-        $.each(data, function(index,value){
-            if(value.length === 0){
-                $('#noData').show();
-            }else{
-                for (let i=0; i< value.length; i++) {
-                    html += '<div class="col-12">' + '<b>' + '<a href="#" class="text-dark">' + value[i].name + '</a>' + '</b>' + '<span class="float-right">' + '(' + value[i].qty +  ' Followers)' + '</span>' + '<hr>' + '</div>'
+        type: "get",
+        dataType: "json",
+        beforeSend: function () {
+            $(".ajax-loading-2").show();
+        },
+    }).done(function (data) {
+        $(".ajax-loading-2").hide();
+        $.each(data, function (index, value) {
+            if (value.length === 0) {
+                $("#noData").show();
+            } else {
+                for (let i = 0; i < value.length; i++) {
+                    html +=
+                        '<div class="col-12">' +
+                        "<b>" +
+                        '<a href="#" class="text-dark">' +
+                        value[i].name +
+                        "</a>" +
+                        "</b>" +
+                        '<span class="float-right">' +
+                        "(" +
+                        value[i].qty +
+                        " Followers)" +
+                        "</span>" +
+                        "<hr>" +
+                        "</div>";
                 }
-                $("#showTopicsHtml").html(html); 
+                $("#showTopicsHtml").html(html);
             }
-            
         });
-        
-    })
-})
+    });
+});
 
-$(document).on('click','#showQuestions',function(e){
-    $('#noData').hide();
+$(document).on("click", "#showQuestions", function (e) {
+    $("#noData").hide();
     $("#showAnswersHtml").hide();
     $("#showTopicsHtml").hide();
     $("#showQuestionsHtml").show();
     e.preventDefault();
-    let site_url = $(this).attr('data-href');
+    let site_url = $(this).attr("data-href");
     let html2 = "";
     $.ajax({
         url: site_url,
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function(){
-            $('.ajax-loading-2').show();
-        }
-    })
-    .done(function(data){
-        $('.ajax-loading-2').hide();
-        $.each(data, function(index,value){
-            if(value.length === 0){
-                $('#noData').show();
-            }else{
-                for (let i=0; i< value.length; i++) {
+        type: "get",
+        dataType: "json",
+        beforeSend: function () {
+            $(".ajax-loading-2").show();
+        },
+    }).done(function (data) {
+        $(".ajax-loading-2").hide();
+        $.each(data, function (index, value) {
+            if (value.length === 0) {
+                $("#noData").show();
+            } else {
+                for (let i = 0; i < value.length; i++) {
                     let title_slug = value[i].title_slug;
-                    html2 += '<div class="col-12">' + '<b>' + '<a href="/' + title_slug + '" class="text-dark">' + value[i].title + '</a>' + '</b>' + '<hr>' + '</div>'
+                    html2 +=
+                        '<div class="col-12">' +
+                        "<b>" +
+                        '<a href="/' +
+                        title_slug +
+                        '" class="text-dark">' +
+                        value[i].title +
+                        "</a>" +
+                        "</b>" +
+                        "<hr>" +
+                        "</div>";
                 }
-                $("#showQuestionsHtml").html(html2); 
+                $("#showQuestionsHtml").html(html2);
             }
-            
         });
-        
-    })
-})
+    });
+});
 
-
-$(document).on('click','#showAnswers',function(e){
-    $('#noData').hide();
+$(document).on("click", "#showAnswers", function (e) {
+    $("#noData").hide();
     $("#showQuestionsHtml").hide();
     $("#showTopicsHtml").hide();
     $("#showAnswersHtml").show();
     e.preventDefault();
-    let site_url = $(this).attr('data-href');
+    let site_url = $(this).attr("data-href");
     let html3 = "";
     $.ajax({
         url: site_url,
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function(){
-            $('.ajax-loading-2').show();
-        }
-    })
-    .done(function(data){
-        let name_slug = $('#name').attr('data-attr');
-        $('.ajax-loading-2').hide();
-        $.each(data, function(index,value){
-            if(value.length === 0){
-                $('#noData').show();
-            }else{
-                for (let i=0; i< value.length; i++) {
+        type: "get",
+        dataType: "json",
+        beforeSend: function () {
+            $(".ajax-loading-2").show();
+        },
+    }).done(function (data) {
+        let name_slug = $("#name").attr("data-attr");
+        $(".ajax-loading-2").hide();
+        $.each(data, function (index, value) {
+            if (value.length === 0) {
+                $("#noData").show();
+            } else {
+                for (let i = 0; i < value.length; i++) {
                     let title_slug = value[i].question.title_slug;
                     let title = value[i].question.title;
-     
-                    html3 += '<div class="col-12">' + '<b>' + '<a href="/' + title_slug + '#'+ name_slug +'" class="text-dark">' + title + '</a>' + '</b>' + '<br>' + value[i].text + '<hr>' + '</div>';
+
+                    html3 +=
+                        '<div class="col-12">' +
+                        "<b>" +
+                        '<a href="/' +
+                        title_slug +
+                        "#" +
+                        name_slug +
+                        '" class="text-dark">' +
+                        title +
+                        "</a>" +
+                        "</b>" +
+                        "<br>" +
+                        value[i].text +
+                        "<hr>" +
+                        "</div>";
                 }
-                $("#showAnswersHtml").html(html3); 
+                $("#showAnswersHtml").html(html3);
             }
-            
         });
-        
-    })
-})
+    });
+});
