c172e2f6c8a208ac2f8c05dc55ae3c3c6c87948a
izzanka <izzancoc.46@gmail.com>
2021-12-25 18:46:25 +0700

    add timestamps on topic migration

--- database/migrations/2021_07_21_154459_create_topics_table.php
+++ database/migrations/2021_07_21_154459_create_topics_table.php
@@ -19,6 +19,7 @@ class CreateTopicsTable extends Migration
             $table->string('name_slug',20);
             $table->integer('follower')->default(0);
             $table->integer('status')->default(0);
+            $table->timestamps();
         });
     }
 
