fb9b47e7f1c0b802ef95da0fcee48413349d71a0
izzanka <izzancoc.46@gmail.com>
2022-01-02 19:33:19 +0700

    add meta desc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Luora is a Quora website clone. It's a platform to ask questions and connect with people who contribute quality answers">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
